69d987f219c8484b16a5b35370fab3742223afb9
dreamtraveller1314 <xinru2509@gmail.com>
2026-08-21 02:55:48 +0800

    Module 1 - 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -5,10 +5,19 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 //@Autonomous
+
 public class HelloWorld extends OpMode {
     @Override
     public void init() {
         telemetry.addData("Hello","World");
+        int teamNumber = 23014;
+        double motorSpeed = 0.75;
+        boolean clawClosed = true;
+        String teamName = "The Flying Dutchman";
+        telemetry.addData("Team Number", teamNumber);
+        telemetry.addData("motor speed", motorSpeed);
+        telemetry.addData("claw closed", clawClosed);
+        telemetry.addData("Name", teamName);
     }
 
     @Override
