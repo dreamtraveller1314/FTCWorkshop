2540fe86a4b9067fe5beb5f37fb92b83e2801caa
dreamtraveller1314 <xinru2509@gmail.com>
2026-08-22 11:31:58 +0800

    variable and ifelse

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -9,7 +9,22 @@ public class HelloWorld extends OpMode {
     @Override
     public void init(){
         telemetry.addData("Hello", "World");
-    }
+        int teamNumber = 28028;
+        double motorSpeed = 0.75;
+        boolean clawStatus = false;
+        String teamName = "ABC Team";
+        telemetry.addData ("Team Number", teamNumber);
+        telemetry.addData ("Motor Speed", motorSpeed);
+        telemetry.addData ("Claw Open?", clawStatus);
+        telemetry.addData ("Team Name", teamName);
+        if (motorSpeed>0){
+            telemetry.addData ("Motor Direction", "Forward");
+        } else-if(motorSpeed<0){
+            telemetry.addData ("Motor Direction", "Backward");
+        }else{
+            telemetry.addData ("Motor Direction", "Stop");
+        }
+    }//dont forget to add motordirection variable
     public void loop(){
 
     }
